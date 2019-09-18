ad5722113e33f1ce87a5299cce58ee0ccfb63e74
unknown <thalissonmoreirasinezio@gmail.com>
2019-09-18 15:05:27 -0300

    Renamed the project folder and added some answers

--- SolvedAssignment/src/aulaprogramacao/AulaProgramacao.java
+++ SolvedAssignment/src/aulaprogramacao/AulaProgramacao.java
@@ -9,11 +9,9 @@ public class AulaProgramacao {
     static Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) {
-   
-        q18();
+        q26();
     }
     static void q1(){
-        
        System.out.println("Digite o Primeiro Valor: ");
        double primeiroValor = sc.nextDouble();
        
@@ -33,9 +31,7 @@ public class AulaProgramacao {
         System.out.println("Divisão: " + divi);
        
     }
-    
     static void q2(){
-    
         System.out.println("Digite a Distância Total Percorrida(Km): ");
         double distanciaTotal = sc.nextDouble();
         
@@ -46,7 +42,6 @@ public class AulaProgramacao {
         System.out.println("Consumo Médio: " + consumoMedio);
         
     }
-    
     static void q3(){
         System.out.println("Digite o Nome do Vendedor: ");
         String nome = sc.nextLine();
@@ -73,7 +68,6 @@ public class AulaProgramacao {
         
     }
     static void q5(){
-    
         System.out.println("Digite Quantos Dollares Tem: ");
         double quantidadeDollar = sc.nextDouble();
         
@@ -84,7 +78,6 @@ public class AulaProgramacao {
         System.out.println("Convertendo Para Reais Fica: " + quantidadeReal);
     }
     static void q6(){
-        
         System.out.println("Digite Quanto Foi Depositado: ");
         double depositado = sc.nextDouble();
         
@@ -92,7 +85,6 @@ public class AulaProgramacao {
         System.out.println("Deposito Com o Rendimento: " + rendimento);
     }
     static void q7(){
-        
         System.out.println("Informe o Valor Final: ");
         double valorFinal = sc.nextDouble();
         
@@ -102,7 +94,6 @@ public class AulaProgramacao {
         
     }
     static void q8(){
-    
         System.out.println("Digite o Custo do Produto: ");
         double custoProduto = sc.nextDouble();
         
@@ -113,7 +104,6 @@ public class AulaProgramacao {
         System.out.println("Valor Final: " + valorFinal);
     }
     static void q9(){
-        
         System.out.println("Digite o Custo de Fábrica: ");
         double custoFabrica = sc.nextDouble();
         
@@ -121,7 +111,6 @@ public class AulaProgramacao {
         System.out.println("Valor Final: " + valorFinal);
     }
     static void q10(){
-        
         int a = 10;
         int b = 20;
         int temp;
@@ -137,7 +126,6 @@ public class AulaProgramacao {
         
     }
     static void q14(){
-        
         System.out.println("Digite Um Valor: ");
         int valor = sc.nextInt();
         
@@ -146,7 +134,6 @@ public class AulaProgramacao {
         
     }
     static void q15(){
-    
         System.out.println("Digite a Base: ");
         int base = sc.nextInt();
         
@@ -170,7 +157,6 @@ public class AulaProgramacao {
         System.out.println("Quantidade Total em Dias: " + diasFinal);
     }
     static void q17(){
-    
         System.out.println("Total de Eleitores: ");
         int totalEleitores = sc.nextInt();
         
@@ -190,25 +176,116 @@ public class AulaProgramacao {
         System.out.println("\nPercentagem Votos em Branco: " + brancoPercentagem + "%");
         System.out.println("Percentagem Votos Nulos: " + nulosPercentagem + "%");
         System.out.println("Percentagem Votos Válidos: " + validosPercentagem + "%");
-        
     }
     static void q18(){
-    
         System.out.println("Digite o Salário Atual: ");
         double salarioAtual = sc.nextDouble();
         
         System.out.println("Digite o Reajuste(%): ");
-        float reajuste = sc.nextFloat();
+        float reajuste = sc.nextFloat()/100;
         
-        double salarioFinal = salarioAtual + (salarioAtual*(reajuste/100));
+        double salarioFinal = salarioAtual + (salarioAtual*reajuste);
         System.out.println("Salário Final Com Reajuste: " + salarioFinal);
     }
     static void q19(){
-        
         System.out.println("Digite o Custo de Fábrica: ");
         double custoFabrica = sc.nextDouble();
         
         double valorFinal = (custoFabrica+(custoFabrica*0.45)) + ((custoFabrica+(custoFabrica*0.45))*0.28);
         System.out.println("Valor Final: " + valorFinal);
+    }
+    static void q20(){
+        System.out.println("Digite a Quantidade de Carros Vendidos: ");
+        int carrosVendidos = sc.nextInt();
+        
+        System.out.println("Digite o Total de Vendas(R$): ");
+        double valorVendas = sc.nextDouble();
+        
+        System.out.println("Digite o Salário Fixo: ");
+        double salarioFixo = sc.nextDouble();
+        
+        System.out.println("Digite a Comissão Por Carro Vendido(%): ");
+        float comissao = sc.nextFloat()/100;
+        
+        double salarioFinal = salarioFixo + (carrosVendidos*comissao) + (valorVendas*0.5);
+        System.out.println("Salario Final: " + salarioFinal);
+    }
+    static void q21(){
+        System.out.println("Digite Um Valor: ");
+        float valor = sc.nextFloat();
+        
+        if(valor>10){
+            System.out.println("É MAIOR QUE 10!");
+        }else{
+            System.out.println("NÃO É MAIOR QUE 10!");
+        }
+    }
+    static void q22(){
+        System.out.println("Digite Um Valor: ");
+        float valor = sc.nextFloat();
+        
+        if(valor>=0){
+            System.out.println("Valor Positivo!");
+        }else{
+            System.out.println("Valor Negativo!");
+        }
+    }
+    static void q23(){
+        System.out.println("Digite a Quantidade de Maçãs: ");
+        int quantidadeMaca = sc.nextInt();
+        
+        double precoMaca = (quantidadeMaca>=12)?1:1.30;
+        double precoFinal = quantidadeMaca * precoMaca;
+        
+        System.out.println("Preço Final: " + precoFinal);
+        
+    }
+    static void q24(){
+        System.out.println("Digite a Primeira Nota: ");
+        float primeiraNota = sc.nextFloat();
+        
+        System.out.println("Digite a Segunda Nota");
+        float segundaNota = sc.nextFloat();
+        
+        float media = (primeiraNota + segundaNota)/2;
+        if(media>=6){
+            System.out.println("Aprovado!");
+            System.out.println("Média: " + media);
+        }else{
+            System.out.println("Reprovado!");
+            System.out.println("Média: " + media);
+        }
+    }
+    static void q25(){
+        System.out.println("Digite o Ano de Nascimento: ");
+        int anoNascimento = sc.nextInt();
+        
+        System.out.println("Digite o Ano Atual: ");
+        int anoAtual = sc.nextInt();
+        
+        if((anoAtual - anoNascimento)>=16){
+            System.out.println("Já Pode Votar!");
+        }else{
+            System.out.println("Não Pode Votar!");
+        }
+    }
+    static void q26(){
+        System.out.println("Escreva Um Valor: ");
+        float valorUm = sc.nextFloat();
+        
+        System.out.println("Escreva Outro Valor: ");
+        float valorDois = sc.nextFloat();
+        
+        System.out.println((valorUm>valorDois)?valorUm:valorDois);
+        
+    }
+    static void q27(){
+        System.out.println("Escreva Um Valor: ");
+        float valorUm = sc.nextFloat();
+        
+        System.out.println("Escreva Outro Valor: ");
+        float valorDois = sc.nextFloat();
+        
+        
     }
 }
